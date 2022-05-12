d3b3fd6f71f9539e59d8c24f762beea4a90ecff5
sefa kucuk <sefa.234@gmail.com>
2022-05-12 12:35:00 +0300

    Data katmanı modeller yazıldı.

--- KafeBerlin.Ui/GecmisSiparislerForm.Designer.cs
+++ KafeBerlin.Ui/GecmisSiparislerForm.Designer.cs
@@ -108,7 +108,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(762, 519);
+            this.ClientSize = new System.Drawing.Size(762, 475);
             this.Controls.Add(this.splitContainer1);
             this.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Name = "GecmisSiparislerForm";
